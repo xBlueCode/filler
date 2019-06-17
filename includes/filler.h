dde34b845ac2eb1be0de5ce8155bb3074ea2f97a
xbluecode <besbes.af@gmail.com>
2019-06-17 06:48:18 +0300

    Updating the interface

--- includes/filler.h
+++ includes/filler.h
@@ -13,4 +13,53 @@
 #ifndef FILLER_H
 # define FILLER_H
 
+#include "utils.h"
+
+# define FI_IN_PL_HEAD "$$$ exec p"
+# define FI_IN_PL_HEAD_LEN 10
+
+# define FI_IN_PL_OFF_I 10
+
+# define FI_IN_PL_OFF_N 15
+
+# define FI_IN_FD 0
+
+# define FI_CE_PL1 'O'
+# define FI_CE_PL2 'X'
+# define FI_CE_EMP '.'
+# define FI_CE_PIE '*'
+
+typedef struct	s_player
+{
+	int		id;
+	char	ch;
+	char	*name;
+}				t_player;
+
+typedef	struct	s_game
+{
+	t_player	me;
+	t_player	en;
+	int 		**map;
+	int 		mnl;
+	int 		mnc;
+	int 		**piece;
+	int			pnl;
+	int 		pnc;
+	t_cell		lastmove;
+}				t_game;
+
+int 			fi_game_run(void);
+int 			fi_game_init(t_game *game);
+int 			fi_game_clean(t_game *game);
+int 			fi_game_play(t_game *game);
+
+int				fi_read_player(t_game *game);
+int 			fi_read_map(t_game *game);
+int 			fi_read_piece(t_game *game);
+
+int 			fi_solve(t_game *game);
+
+int 			fi_print_lastmove(t_game *game);
+
 #endif
