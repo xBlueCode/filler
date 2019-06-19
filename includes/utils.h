1460218882734696d891aeb2eb2810a44bc1d030
xbluecode <besbes.af@gmail.com>
2019-06-20 02:27:13 +0300

    Implementing the fi_log_mtx_*

--- includes/utils.h
+++ includes/utils.h
@@ -6,5 +6,7 @@
 #define UTILS_H
 
 int 	**ut_read_mtx(int fd, int nl, int nc, int off);
+int 	**ut_log_mtx_int(int **mtx, int nl, int nc);
+int 	**ut_log_mtx_ch(int **mtx, int nl, int nc);
 
 #endif
--- src/utils/ut_mtx.c
+++ src/utils/ut_mtx.c
@@ -9,6 +9,7 @@ int 	**ut_read_mtx(int fd, int nl, int nc, int off)
 	char	*line;
 	int		**mtx;
 	int		i;
+	int 	j;
 
 	if (fd < 0 || nl <= 0 || nc <= 0)
 		return (NULL);
@@ -21,9 +22,46 @@ int 	**ut_read_mtx(int fd, int nl, int nc, int off)
 			ft_arr_free((void***)&mtx, nc);
 			return (NULL);
 		}
-		mtx[i] = (int*)ft_strndup(line + off, nc);
+		mtx[i] = ft_memalloc((nc + 1) * sizeof(int));
+		j = -1;
+		while (++j < nc)
+			mtx[i][j] = line[j + off];
+		mtx[i][j] = 0;
 		FT_MEMDEL(line);
 	}
 	mtx[i] = NULL;
 	return (mtx);
+}
+
+int 	**ut_log_mtx_int(int **mtx, int nl, int nc)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (++i < nl)
+	{
+		j = -1;
+		FT_LOG(FT_LOG_LINF, FT_LOG_FMESS, "%03d >>  ", i);
+		while (++j < nc)
+			FT_LOG(FT_LOG_LINF, FT_LOG_FMESS, "%-4d", mtx[i][j]);
+		FT_LOG(FT_LOG_LINF, FT_LOG_FMESS, "%c", '\n');
+	}
+	return (OK);
+}
+
+int 	**ut_log_mtx_ch(int **mtx, int nl, int nc)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (++i < nl)
+	{
+		j = -1;
+		while (++j < nc)
+		FT_LOG(FT_LOG_LINF, FT_LOG_FMESS, " %c ", mtx[i][j]);
+		FT_LOG(FT_LOG_LINF, FT_LOG_FMESS, " %c", '\n');
+	}
+	return (OK);
 }
